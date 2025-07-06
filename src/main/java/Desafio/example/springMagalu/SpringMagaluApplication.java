fcc12c1d89beb508b97d93119f6b1e08b1e02257
PedrodeAndradecf <pedro.cfandrade@gmail.com>
2025-07-06 17:22:41 -0300

    Atualizando Pastas

---
+++ src/main/java/Desafio/example/springMagalu/SpringMagaluApplication.java
@@ -0,0 +1,13 @@
+package Desafio.example.springMagalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Desafio/example/springMagalu/SpringMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio.example.springMagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
